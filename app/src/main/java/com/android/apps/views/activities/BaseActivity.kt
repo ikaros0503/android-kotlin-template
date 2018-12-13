216ff119ac123432166f46657f417e4cf103ddf8
An Nguyen <ikaros.spacespeaker@gmail.com>
2018-12-13 13:03:11 +0700

    Change something

---
+++ app/src/main/java/com/android/apps/views/activities/BaseActivity.kt
@@ -0,0 +1,52 @@
+package com.android.apps.views.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.android.apps.R
+import com.android.apps.extensions.toast
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+abstract class BaseActivity: AppCompatActivity() {
+
+    private var disposableBackPressed: Disposable? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayoutId())
+        initialize()
+    }
+
+    abstract fun getLayoutId(): Int
+
+    protected open fun initialize() {
+        initializeVariable()
+        initializeViewComponent()
+    }
+
+    fun navigateTo(activity: Class<*>, bundle: Bundle = Bundle()) {
+        startActivity(Intent(this, activity), bundle)
+    }
+
+    protected open fun initializeVariable() {}
+
+    protected open fun initializeViewComponent() {}
+
+    protected open val enablePressAgainToExit: Boolean = false
+
+    override fun onBackPressed() {
+        if (!enablePressAgainToExit)
+            return super.onBackPressed()
+
+        disposableBackPressed?.takeIf { it.isDisposed.not() }?.let {
+            it.dispose()
+            super.onBackPressed()
+        } ?: run {
+            toast(messageId = R.string.text_press_back_again_to_exit)
+            disposableBackPressed = Observable.timer(2000, TimeUnit.MILLISECONDS).subscribe()
+        }
+    }
+}
